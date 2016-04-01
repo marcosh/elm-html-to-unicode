68fd3edc4b232a7baba26bc0a5869c8ecd94c73c
Marco Perone <m.perone@mvlabs.it>
2016-04-01 23:07:43 +0200

    some improvements:
    - export just convert function
    - start a little documentation
    - remove Debug
    - refactor convertNumericalCode and convertFriendlyCode
    - simplify friendlyConverterDictionary data structure

--- HtmlToUnicode.elm
+++ HtmlToUnicode.elm
@@ -1,13 +1,17 @@
-module HtmlToUnicode where
+module HtmlToUnicode (convert) where
 
-import List
+{-|
+@docs convert
+-}
+
 import String
-import Maybe
 import Char
 import Dict
-import Debug
+import Result
 
 
+{-|
+-}
 convert : String -> String
 convert string = string
     |> String.toList -- convert to a list of chars
@@ -65,133 +69,132 @@ noAmpUnicodeConverter pre post list =
                 convertFriendlyCode [ pre ] [ post ] ( head::tail )
 
 
-convertNumericalCode : List Char -> List Char -> List Char -> List Char
-convertNumericalCode pre post list =
+convertCode : ( String -> Maybe a ) -> ( a -> List Char ) -> List Char -> List Char -> List Char -> List Char
+convertCode mayber lister pre post list =
     let
         string = String.fromList list
-        keyCodeResult = String.toInt string
+        maybe = mayber string
     in
-        case keyCodeResult of
-            Err _ -> List.concat [ pre, list, post ]
-            Ok keyCode -> [ Char.fromCode keyCode ]
+        case maybe of
+            Nothing -> List.concat [ pre, list, post ]
+            Just something -> lister something
+
+
+convertNumericalCode : List Char -> List Char -> List Char -> List Char
+convertNumericalCode =
+    convertCode ( String.toInt >> Result.toMaybe ) ( \int -> [ Char.fromCode int ])
 
 
 convertFriendlyCode : List Char -> List Char -> List Char -> List Char
-convertFriendlyCode pre post list =
-    let
-        string = String.fromList list
-        charMaybe = convertFriendlyCodeToChar string
-    in
-        case charMaybe of
-            Nothing -> List.concat [ pre, list, post ]
-            Just char -> [ char ]
+convertFriendlyCode =
+    convertCode convertFriendlyCodeToChar ( \char -> [ char ])
 
 
 convertFriendlyCodeToChar : String -> Maybe Char
-convertFriendlyCodeToChar string = Dict.get (Debug.log "string "string) friendlyConverterDictionary
+convertFriendlyCodeToChar string = Dict.get string friendlyConverterDictionary
 
 
-friendlyConverterDictionary : Dict.Dict String Char
-friendlyConverterDictionary
-    = Dict.empty
-    |> Dict.insert "quot" ( Char.fromCode 34 )
-    |> Dict.insert "amp" ( Char.fromCode 38 )
-    |> Dict.insert "lt" ( Char.fromCode 60 )
-    |> Dict.insert "gt" ( Char.fromCode 62 )
-    |> Dict.insert "nbsp" ( Char.fromCode 160 )
-    |> Dict.insert "iexcl" ( Char.fromCode 161 )
-    |> Dict.insert "cent" ( Char.fromCode 162 )
-    |> Dict.insert "pound" ( Char.fromCode 163 )
-    |> Dict.insert "curren" ( Char.fromCode 164 )
-    |> Dict.insert "yen" ( Char.fromCode 165 )
-    |> Dict.insert "brvbar" ( Char.fromCode 166 )
-    |> Dict.insert "sect" ( Char.fromCode 167 )
-    |> Dict.insert "uml" ( Char.fromCode 168 )
-    |> Dict.insert "copy" ( Char.fromCode 169 )
-    |> Dict.insert "ordf" ( Char.fromCode 170 )
-    |> Dict.insert "laquo" ( Char.fromCode 171 )
-    |> Dict.insert "not" ( Char.fromCode 172 )
-    |> Dict.insert "shy" ( Char.fromCode 173 )
-    |> Dict.insert "reg" ( Char.fromCode 174 )
-    |> Dict.insert "macr" ( Char.fromCode 175 )
-    |> Dict.insert "deg" ( Char.fromCode 176 )
-    |> Dict.insert "plusmn" ( Char.fromCode 177 )
-    |> Dict.insert "sup2" ( Char.fromCode 178 )
-    |> Dict.insert "sup3" ( Char.fromCode 179 )
-    |> Dict.insert "acute" ( Char.fromCode 180 )
-    |> Dict.insert "micro" ( Char.fromCode 181 )
-    |> Dict.insert "para" ( Char.fromCode 182 )
-    |> Dict.insert "middot" ( Char.fromCode 183 )
-    |> Dict.insert "cedil" ( Char.fromCode 184 )
-    |> Dict.insert "sup1" ( Char.fromCode 185 )
-    |> Dict.insert "ordm" ( Char.fromCode 186 )
-    |> Dict.insert "raquo" ( Char.fromCode 187 )
-    |> Dict.insert "frac14" ( Char.fromCode 188 )
-    |> Dict.insert "frac12" ( Char.fromCode 189 )
-    |> Dict.insert "frac34" ( Char.fromCode 190 )
-    |> Dict.insert "iquest" ( Char.fromCode 191 )
-    |> Dict.insert "Agrave" ( Char.fromCode 192 )
-    |> Dict.insert "Aacute" ( Char.fromCode 193 )
-    |> Dict.insert "Acirc" ( Char.fromCode 194 )
-    |> Dict.insert "Atilde" ( Char.fromCode 195 )
-    |> Dict.insert "Auml" ( Char.fromCode 196 )
-    |> Dict.insert "Aring" ( Char.fromCode 197 )
-    |> Dict.insert "AElig" ( Char.fromCode 198 )
-    |> Dict.insert "Ccedil" ( Char.fromCode 199 )
-    |> Dict.insert "Egrave" ( Char.fromCode 200 )
-    |> Dict.insert "Eacute" ( Char.fromCode 201 )
-    |> Dict.insert "Ecirc" ( Char.fromCode 202 )
-    |> Dict.insert "Euml" ( Char.fromCode 203 )
-    |> Dict.insert "Igrave" ( Char.fromCode 204 )
-    |> Dict.insert "Iacute" ( Char.fromCode 205 )
-    |> Dict.insert "Icirc" ( Char.fromCode 206 )
-    |> Dict.insert "Iuml" ( Char.fromCode 207 )
-    |> Dict.insert "ETH" ( Char.fromCode 208 )
-    |> Dict.insert "Ntilde" ( Char.fromCode 209 )
-    |> Dict.insert "Ograve" ( Char.fromCode 210 )
-    |> Dict.insert "Oacute" ( Char.fromCode 211 )
-    |> Dict.insert "Ocirc" ( Char.fromCode 212 )
-    |> Dict.insert "Otilde" ( Char.fromCode 213 )
-    |> Dict.insert "Ouml" ( Char.fromCode 214 )
-    |> Dict.insert "times" ( Char.fromCode 215 )
-    |> Dict.insert "Oslash" ( Char.fromCode 216 )
-    |> Dict.insert "Ugrave" ( Char.fromCode 217 )
-    |> Dict.insert "Uacute" ( Char.fromCode 218 )
-    |> Dict.insert "Ucirc" ( Char.fromCode 219 )
-    |> Dict.insert "Uuml" ( Char.fromCode 220 )
-    |> Dict.insert "Yacute" ( Char.fromCode 221 )
-    |> Dict.insert "THORN" ( Char.fromCode 222 )
-    |> Dict.insert "szlig" ( Char.fromCode 223 )
-    |> Dict.insert "agrave" ( Char.fromCode 224 )
-    |> Dict.insert "aacute" ( Char.fromCode 225 )
-    |> Dict.insert "acirc" ( Char.fromCode 226 )
-    |> Dict.insert "atilde" ( Char.fromCode 227 )
-    |> Dict.insert "auml" ( Char.fromCode 228 )
-    |> Dict.insert "aring" ( Char.fromCode 229 )
-    |> Dict.insert "aelig" ( Char.fromCode 230 )
-    |> Dict.insert "ccedil" ( Char.fromCode 231 )
-    |> Dict.insert "egrave" ( Char.fromCode 232 )
-    |> Dict.insert "eacute" ( Char.fromCode 233 )
-    |> Dict.insert "ecirc" ( Char.fromCode 234 )
-    |> Dict.insert "euml" ( Char.fromCode 235 )
-    |> Dict.insert "igrave" ( Char.fromCode 236 )
-    |> Dict.insert "iacute" ( Char.fromCode 237 )
-    |> Dict.insert "icirc" ( Char.fromCode 238 )
-    |> Dict.insert "iuml" ( Char.fromCode 239 )
-    |> Dict.insert "eth" ( Char.fromCode 240 )
-    |> Dict.insert "ntilde" ( Char.fromCode 241 )
-    |> Dict.insert "ograve" ( Char.fromCode 242 )
-    |> Dict.insert "oacute" ( Char.fromCode 243 )
-    |> Dict.insert "ocirc" ( Char.fromCode 244 )
-    |> Dict.insert "otilde" ( Char.fromCode 245 )
-    |> Dict.insert "ouml" ( Char.fromCode 246 )
-    |> Dict.insert "divide" ( Char.fromCode 247 )
-    |> Dict.insert "oslash" ( Char.fromCode 248 )
-    |> Dict.insert "ugrave" ( Char.fromCode 249 )
-    |> Dict.insert "uacute" ( Char.fromCode 250 )
-    |> Dict.insert "ucirc" ( Char.fromCode 251 )
-    |> Dict.insert "uuml" ( Char.fromCode 252 )
-    |> Dict.insert "yacute" ( Char.fromCode 253 )
-    |> Dict.insert "thorn" ( Char.fromCode 254 )
-    |> Dict.insert "yuml" ( Char.fromCode 255 )
-    |> Dict.insert "euro" ( Char.fromCode 8364 )
+friendlyConverterDictionary =
+    Dict.fromList <| List.map (\(a,b) -> (a, Char.fromCode b))
+        [ ( "quot", 34 )
+        , ( "amp", 38 )
+        , ( "lt", 60 )
+        , ( "gt", 62 )
+        , ( "nbsp", 160 )
+        , ( "iexcl", 161 )
+        , ( "cent", 162 )
+        , ( "pound", 163 )
+        , ( "curren", 164 )
+        , ( "yen", 165 )
+        , ( "brvbar", 166 )
+        , ( "sect", 167 )
+        , ( "uml", 168 )
+        , ( "copy", 169 )
+        , ( "ordf", 170 )
+        , ( "laquo", 171 )
+        , ( "not", 172 )
+        , ( "shy", 173 )
+        , ( "reg", 174 )
+        , ( "macr", 175 )
+        , ( "deg", 176 )
+        , ( "plusmn", 177 )
+        , ( "sup2", 178 )
+        , ( "sup3", 179 )
+        , ( "acute", 180 )
+        , ( "micro", 181 )
+        , ( "para", 182 )
+        , ( "middot", 183 )
+        , ( "cedil", 184 )
+        , ( "sup1", 185 )
+        , ( "ordm", 186 )
+        , ( "raquo", 187 )
+        , ( "frac14", 188 )
+        , ( "frac12", 189 )
+        , ( "frac34", 190 )
+        , ( "iquest", 191 )
+        , ( "Agrave", 192 )
+        , ( "Aacute", 193 )
+        , ( "Acirc", 194 )
+        , ( "Atilde", 195 )
+        , ( "Auml", 196 )
+        , ( "Aring", 197 )
+        , ( "AElig", 198 )
+        , ( "Ccedil", 199 )
+        , ( "Egrave", 200 )
+        , ( "Eacute", 201 )
+        , ( "Ecirc", 202 )
+        , ( "Euml", 203 )
+        , ( "Igrave", 204 )
+        , ( "Iacute", 205 )
+        , ( "Icirc", 206 )
+        , ( "Iuml", 207 )
+        , ( "ETH", 208 )
+        , ( "Ntilde", 209 )
+        , ( "Ograve", 210 )
+        , ( "Oacute", 211 )
+        , ( "Ocirc", 212 )
+        , ( "Otilde", 213 )
+        , ( "Ouml", 214 )
+        , ( "times", 215 )
+        , ( "Oslash", 216 )
+        , ( "Ugrave", 217 )
+        , ( "Uacute", 218 )
+        , ( "Ucirc", 219 )
+        , ( "Uuml", 220 )
+        , ( "Yacute", 221 )
+        , ( "THORN", 222 )
+        , ( "szlig", 223 )
+        , ( "agrave", 224 )
+        , ( "aacute", 225 )
+        , ( "acirc", 226 )
+        , ( "atilde", 227 )
+        , ( "auml", 228 )
+        , ( "aring", 229 )
+        , ( "aelig", 230 )
+        , ( "ccedil", 231 )
+        , ( "egrave", 232 )
+        , ( "eacute", 233 )
+        , ( "ecirc", 234 )
+        , ( "euml", 235 )
+        , ( "igrave", 236 )
+        , ( "iacute", 237 )
+        , ( "icirc", 238 )
+        , ( "iuml", 239 )
+        , ( "eth", 240 )
+        , ( "ntilde", 241 )
+        , ( "ograve", 242 )
+        , ( "oacute", 243 )
+        , ( "ocirc", 244 )
+        , ( "otilde", 245 )
+        , ( "ouml", 246 )
+        , ( "divide", 247 )
+        , ( "oslash", 248 )
+        , ( "ugrave", 249 )
+        , ( "uacute", 250 )
+        , ( "ucirc", 251 )
+        , ( "uuml", 252 )
+        , ( "yacute", 253 )
+        , ( "thorn", 254 )
+        , ( "yuml", 255 )
+        , ( "euro", 8364 )
+        ]
